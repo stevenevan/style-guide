b56e657c8e7471025cae525d993e116ba4034362
Steven Evan <steven.evan999@gmail.com>
2025-03-26 09:29:23 +0700

    fix: unicorn flat

--- eslint/flat/unicorn.js
+++ eslint/flat/unicorn.js
@@ -3,9 +3,10 @@ const unicorn = require('eslint-plugin-unicorn');
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   plugins: {
-    unicorn,
+    unicorn: unicorn,
   },
   rules: {
+    ...unicorn.configs.recommended.rules,
     ...require('../rules/unicorn').rules,
   },
 };
