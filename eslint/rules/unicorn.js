75e2af1f631684063357bfbf790446f6e7f53674
Steven Evan <steven.evan999@gmail.com>
2025-07-10 10:37:23 +0700

    chore: remove unicorn filenamecase

--- eslint/rules/unicorn.js
+++ eslint/rules/unicorn.js
@@ -1,17 +1,6 @@
 module.exports = {
   rules: {
     /**
-     * Require consistent filename case for all linted files.
-     *
-     * 🚫 Not fixable - https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/filename-case.md
-     */
-    'unicorn/filename-case': [
-      'error',
-      {
-        case: 'kebabCase',
-      },
-    ],
-    /**
      * Require using the `node:` protocol when importing Node.js built-in modules.
      *
      * 🔧 Fixable - https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-node-protocol.md
