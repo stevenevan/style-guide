75dd446e8746c3a59010935b97df01ebf813a730
Steven Evan <steven.evan999@gmail.com>
2025-07-10 10:38:25 +0700

    chore: remove entire unicorn rules

--- eslint/rules/unicorn.js
+++ eslint/rules/unicorn.js
@@ -1,10 +1,1 @@
-module.exports = {
-  rules: {
-    /**
-     * Require using the `node:` protocol when importing Node.js built-in modules.
-     *
-     * 🔧 Fixable - https://github.com/sindresorhus/eslint-plugin-unicorn/blob/main/docs/rules/prefer-node-protocol.md
-     */
-    'unicorn/prefer-node-protocol': 'warn',
-  },
-};
+module.exports = {};
